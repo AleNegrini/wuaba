3a7128619fd00ec5f4e00a7f25fcc8b622f30a3d
Alessandro Negrini <alessandro2.negrini@gmail.com>
2022-02-24 08:53:10 +0100

    feat(frontend): add ThemeProvider

---
+++ frontend/src/theme/baseThemeOptions.ts
@@ -0,0 +1,263 @@
+import { ThemeOptions } from "@mui/material/styles";
+import typography from "./typography";
+
+export const baseThemeOptions: Partial<ThemeOptions> = {
+  typography,
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 1000,
+      lg: 1200,
+      xl: 1920,
+    },
+  },
+  components: {
+    MuiAvatar: {
+      styleOverrides: {
+        root: {
+          fontSize: 14,
+          fontWeight: 600,
+          letterSpacing: 0,
+        },
+      },
+    },
+    MuiButton: {
+      defaultProps: {
+        disableElevation: true,
+      },
+      styleOverrides: {
+        root: {
+          textTransform: "none",
+        },
+        sizeSmall: {
+          padding: "6px 16px",
+        },
+        sizeMedium: {
+          padding: "8px 20px",
+        },
+        sizeLarge: {
+          padding: "11px 24px",
+        },
+        textSizeSmall: {
+          padding: "7px 12px",
+        },
+        textSizeMedium: {
+          padding: "9px 16px",
+        },
+        textSizeLarge: {
+          padding: "12px 16px",
+        },
+      },
+    },
+    MuiButtonBase: {
+      defaultProps: {
+        disableRipple: true,
+      },
+    },
+    MuiCardActions: {
+      styleOverrides: {
+        root: {
+          padding: "16px 24px",
+        },
+      },
+    },
+    MuiCardContent: {
+      styleOverrides: {
+        root: {
+          padding: "32px 24px",
+          "&:last-child": {
+            paddingBottom: "32px",
+          },
+        },
+      },
+    },
+    MuiCardHeader: {
+      defaultProps: {
+        titleTypographyProps: {
+          variant: "h6",
+        },
+        subheaderTypographyProps: {
+          variant: "body2",
+        },
+      },
+      styleOverrides: {
+        root: {
+          padding: "32px 24px",
+        },
+      },
+    },
+    MuiCheckbox: {
+      defaultProps: {
+        color: "primary",
+      },
+    },
+    MuiCssBaseline: {
+      styleOverrides: {
+        "*": {
+          boxSizing: "border-box",
+        },
+        html: {
+          MozOsxFontSmoothing: "grayscale",
+          WebkitFontSmoothing: "antialiased",
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100%",
+          width: "100%",
+        },
+        body: {
+          display: "flex",
+          flex: "1 1 auto",
+          flexDirection: "column",
+          minHeight: "100%",
+          width: "100%",
+        },
+        "#__next": {
+          display: "flex",
+          flex: "1 1 auto",
+          flexDirection: "column",
+          height: "100%",
+          width: "100%",
+        },
+        "#nprogress": {
+          pointerEvents: "none",
+        },
+        "#nprogress .bar": {
+          backgroundColor: "#5048E5",
+          height: 3,
+          left: 0,
+          position: "fixed",
+          top: 0,
+          width: "100%",
+          zIndex: 2000,
+        },
+      },
+    },
+    MuiIconButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: 8,
+          padding: 8,
+        },
+        sizeSmall: {
+          padding: 4,
+        },
+      },
+    },
+    MuiLinearProgress: {
+      styleOverrides: {
+        root: {
+          borderRadius: 3,
+          overflow: "hidden",
+        },
+      },
+    },
+    MuiLink: {
+      defaultProps: {
+        underline: "always",
+      },
+    },
+    MuiListItemIcon: {
+      styleOverrides: {
+        root: {
+          minWidth: 32,
+          marginRight: "16px",
+          "&.MuiListItemIcon-root": {
+            minWidth: "unset",
+          },
+        },
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        input: {
+          fontWeight: 500,
+        },
+      },
+    },
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          backgroundImage: "none",
+        },
+      },
+    },
+    MuiPopover: {
+      defaultProps: {
+        elevation: 16,
+      },
+    },
+    MuiRadio: {
+      defaultProps: {
+        color: "primary",
+      },
+    },
+    MuiSwitch: {
+      defaultProps: {
+        color: "primary",
+      },
+    },
+    MuiTab: {
+      styleOverrides: {
+        root: {
+          fontSize: 14,
+          fontWeight: 500,
+          lineHeight: 1.71,
+          minWidth: "auto",
+          paddingLeft: 0,
+          paddingRight: 0,
+          textTransform: "none",
+        },
+      },
+    },
+    MuiToolbar: {
+      styleOverrides: {
+        dense: {
+          height: "48px",
+          minHeight: "48px",
+        },
+      },
+    },
+    MuiTableCell: {
+      styleOverrides: {
+        root: {
+          padding: "15px 16px",
+        },
+        head: {
+          borderBottom: "none",
+          fontSize: "12px",
+          fontWeight: 600,
+          lineHeight: 1,
+          letterSpacing: 0.5,
+          textTransform: "uppercase",
+        },
+        paddingCheckbox: {
+          paddingTop: 4,
+          paddingBottom: 4,
+        },
+      },
+    },
+    MuiTableHead: {
+      styleOverrides: {
+        root: {
+          borderBottom: "none",
+        },
+      },
+    },
+    MuiChip: {
+      styleOverrides: {
+        root: {
+          fontWeight: 500,
+        },
+      },
+    },
+  },
+  direction: "ltr",
+  shape: {
+    borderRadius: 8,
+  },
+  zIndex: {
+    appBar: 1200,
+    drawer: 1100,
+  },
+};
